19bd64dab77c069bfd0643f043d9a1ebc450d83b
JorgeOA200596 <42782892+JorgeOA200596@users.noreply.github.com>
2021-04-26 16:24:42 -0500

    sub rutas registrar y modificar

--- ticketsmoreweb/lib/src/providers/rutas.dart
+++ ticketsmoreweb/lib/src/providers/rutas.dart
@@ -1,12 +1,8 @@
-import 'package:http/http.dart' as _http;
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/foundation.dart';
-import '../models/Routes.model.dart';
 import 'package:http/http.dart' as http;
 import 'baseUrl.dart';
-import 'dart:convert';
 //import 'package:movert_taxista/src/library/globals.dart' as _globals;
 
 class RutasProvider {
@@ -24,6 +20,31 @@ class RutasProvider {
         .catchError((onError) {
           return {"status": false, "mensaje": "coneccion rechazada"};
         });
+    print(response.statusCode);
+    print(response);
+    if (response != null &&
+        response?.statusCode != null &&
+        response.statusCode == 200) {
+      print(response.body);
+      final jsonResponse = json.decode(response.body);
+      return jsonResponse;
+    } else {
+      return {"status": false, "mensaje": "ruta invalido"};
+    }
+  }
+
+  Future<Map<String, dynamic>> getRouteById({@required String idRoute}) async {
+    Uri url = Uri.http(_url.getBaseUrl(),
+        _url.getNextUrl() + "/getRoute/$idRoute"); //del archivo routes
+    print(url);
+    final response = await http
+        .post(url)
+        .timeout(
+          Duration(seconds: 5),
+        )
+        .catchError((onError) {
+      return {"status": false, "mensaje": "coneccion rechazada"};
+    });
     print(response.statusCode);
     print(response);
     if (response != null &&
--- ticketsmoreweb/lib/src/repository/repository.dart
+++ ticketsmoreweb/lib/src/repository/repository.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:ticketsmoreweb/src/providers/geofences.provider.dart';
+import 'package:ticketsmoreweb/src/providers/subroute.provider.dart';
+import 'package:ticketsmoreweb/src/providers/rutas.dart';
 
 class Repository {
   GeofencesProvider _geofencesProvider = GeofencesProvider();
+  SubRutasProvider _subRutasProvider = SubRutasProvider();
+  RutasProvider _rutasProvider = RutasProvider();
 
   Future<Map<String, dynamic>> saveGeofence(
           {Map<String, dynamic> data}) async =>
@@ -10,4 +14,10 @@ class Repository {
 
   Future<dynamic> getAllGeofences() async =>
       await _geofencesProvider.getAllGeofences();
+
+  Future<dynamic> getAllSubRoutes(idRoute) async =>
+      await _subRutasProvider.getSubroute(idSubroute: idRoute);
+
+  Future<dynamic> getDataRoute(idRoute) async =>
+      await _rutasProvider.getRouteById(idRoute: idRoute);
 }
